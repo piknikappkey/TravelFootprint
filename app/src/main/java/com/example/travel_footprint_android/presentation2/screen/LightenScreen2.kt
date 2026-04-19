215e8e4c032c42937ba9e4ae50533e1fc51accb7
Almond Fish <2803796204@qq.com>
2026-04-19 18:23:09 +0800

    重构点亮页面

--- app/src/main/java/com/example/travel_footprint_android/presentation2/screen/LightenScreen2.kt
+++ app/src/main/java/com/example/travel_footprint_android/presentation2/screen/LightenScreen2.kt
@@ -4,15 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.travel_footprint_android.presentation2.components.SVGMap
 import com.example.travel_footprint_android.presentation2.components.light_panel2.LightPanel2
+import com.example.travel_footprint_android.presentation2.components.svg_map.SVGMap
 
 @Composable
 fun LightenScreen2() {
     Column (
         modifier = Modifier.fillMaxSize()
     ) {
-        SVGMap(modifier = Modifier.weight(1f))
+        SVGMap(Modifier.weight(1f))
         LightPanel2()
     }
 }
